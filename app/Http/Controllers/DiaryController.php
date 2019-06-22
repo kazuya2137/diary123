abf0b278f57cba11744bfd0089906c8abc291528
和也山本 <kazuya2137@gmail.com>
2019-06-22 21:41:12 +0900

    追加エラー出た

--- app/Http/Controllers/DiaryController.php
+++ app/Http/Controllers/DiaryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Diary;
+use App\Http\Requests\CreateDiary; // 追加
 
 
 class DiaryController extends Controller
@@ -28,9 +29,39 @@ class DiaryController extends Controller
         // views/diaries/create.blade.phpを表示する
         return view('diaries.create');
     }
-    public function store(Request $request)
+    public function store(CreateDiary $request)
 {
     $diary = new Diary(); //Diaryモデルをインスタンス化
+
+    $diary->title = $request->title; //画面で入力されたタイトルを代入
+    $diary->body = $request->body; //画面で入力された本文を代入
+    $diary->save(); //DBに保存
+
+    return redirect()->route('diary.index'); //一覧ページにリダイレクト
+}
+
+    public function destroy(int $id)
+{
+    //Diaryモデルを使用して、diariesテーブルから$idと一致するidをもつデータを取得
+    $diary = Diary::find($id); 
+
+    //取得したデータを削除
+    $diary->delete();
+
+    return redirect()->route('diary.index');
+}
+public function edit(int $id)
+{
+     //Diaryモデルを使用して、diariesテーブルから$idと一致するidをもつデータを取得
+    $diary = Diary::find($id);
+
+    return view('diaries.edit', [
+        'diary' => $diary,
+    ]);
+}
+public function update(int $id, CreateDiary $request)
+{
+    $diary = Diary::find($id);
 
     $diary->title = $request->title; //画面で入力されたタイトルを代入
     $diary->body = $request->body; //画面で入力された本文を代入
--- resources/views/diaries/index.blade.php
+++ resources/views/diaries/index.blade.php
@@ -1,3 +1,10 @@
+
+{{-- テンプレートエンジン --}}
+{{-- bladeっていうテンプレートエンジンを使用している --}}
+{{-- @if @foreach {{}}->bladeのテンプレートの構文 --}}
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,11 +15,20 @@
     <title>一覧表示画面</title>
 </head>
 <body>
+        <a href="{{ route('diary.create') }}" class="btn btn-primary btn-block">
+            新規投稿
+        </a>
     @foreach ($diaries as $diary)
         <div class="m-4 p-4 border border-primary">
             <p>{{ $diary->title }}</p>
             <p>{{ $diary->body }}</p>
             <p>{{ $diary->created_at }}</p>
+            <a class="btn btn-success" href="{{ route('diary.edit', ['id' => $diary->id]) }}">編集</a>
+            <form action="{{ route('diary.destroy', ['id' => $diary->id]) }}" method="POST" class="d-inline">
+                @csrf
+                @method('delete')
+                <button class="btn btn-danger">削除</button>
+            </form>
         </div>
     @endforeach
 </body>
